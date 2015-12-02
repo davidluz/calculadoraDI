6117ba7ac4bfc3a1b9fc38110f952ca45683b515
davidluz <davidmelo8@hotmail.com>
2015-12-02 18:00:13 -0200

    v 2.0
    Segue

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-var recursosSelecionados = [];
-var app = angular
-  .module('MyApp', [
-  ])
-.controller('QuestionCtrl', ['$scope', '$http', function ($scope, $http) { 
-  $scope.recursos = [];
-  $http
-    .get("data.json")
-    .then(function(response){
-      $scope.recursos = response.data.recursos;
 
-    /* Somente dentro deste escopo os objetos são acessados. */
+var app = angular.module('CalcDi', []).controller('RecursosCtrl', ['$scope', '$http', function ($scope, $http) { 
+$scope.recursos = [];
+$scope.recursosSelecionados = [];
+$http.get("data.json").then(function(response){
+$scope.recursos = response.data.recursos;
 
-    //Pega todos os objetos do Array recursos e coloca na lista
-    for(var i=0; i<$scope.recursos.length; i++){
-    $('#lista-de-recursos').append('<option value="'+ $scope.recursos[i].id+'">'+ $scope.recursos[i].nome+'</option>');
-    }
+/* Somente dentro deste escopo os objetos são acessados. */
+//Pega todos os objetos do Array recursos e coloca na lista
+for(var i=0; i<$scope.recursos.length; i++){
+$('#lista-de-recursos').append('<option value="'+ $scope.recursos[i].id+'">'+ $scope.recursos[i].nome+'</option>');
+$('select').material_select();
+}
 
 
-     
-    });
-       
-       $(document).ready(function() {
-         	
-            
-         	$('#escolher').click(function(event) {
-         		//Guarda o ID do recurso selecionado no array recursos Selecioandos
-         		recursosSelecionados.push($('#lista-de-recursos').val());
-         		console.log(recursosSelecionados);
-         		});
+//Actions dos botões
+$('#escolher').click(function(event) {
+//Guarda o ID do recurso selecionado no array recursos Selecioandos
+$scope.recursosSelecionados.push($('#lista-de-recursos').val());
+});
+
+$("#estimar").click(function(event){
+realizarEstimativa();
+});
 
 
-
-       });
-     
- 
-
+});
 
 }]) 
 
+function realizarEstimativa(){
+console.log('fimDaEstimativa');
+} 
+
+$(document).ready(function() {
+    
+});
 
